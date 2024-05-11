ac5552748cf0518cc049255a0fb3adcb132e0b58
Amrinder Singh <amrinder995@gmail.com>
2024-05-10 20:14:30 -0400

    Create project folder, setup connection to db, add and install dependancies

---
+++ src/lib/users.js
@@ -0,0 +1,12 @@
+// /src/lib/users.js
+import { Users } from "./mongodb";
+
+export const getAllUsers = async () => {
+  const users = await (await Users()).find({}).toArray();
+  return users;
+};
+
+export const createUser = async (newUser) => {
+  const user = await (await Users()).insert(newUser);
+  return user;
+};
---
+++ src/pages/components/CreateUserForm.jsx
@@ -0,0 +1,5 @@
+export default function CreateUserForm() {
+  return (
+    <h1>Users</h1>
+  )
+}
