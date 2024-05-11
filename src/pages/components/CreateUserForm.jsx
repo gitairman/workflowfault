65c95c7f399d3c3d37ad8cc7e174c47fc07487de
Amrinder Singh <amrinder995@gmail.com>
2024-05-10 21:10:38 -0400

    create user form component, nav component

--- src/pages/components/CreateUserForm.jsx
+++ src/pages/components/CreateUserForm.jsx
@@ -1,5 +1,18 @@
 export default function CreateUserForm() {
   return (
-    <h1>Users</h1>
+    <div>
+      <h1 class="text-2xl">Create new user:</h1>
+      <form>
+        <label for="name">Name</label>
+        <input type="text" name="name" required/>
+        <label for="email">Email</label>
+        <input type="email" name="email" required/>
+        <label for="password">Password</label>
+        <input type="password" name="password" required/>
+        <label for="company">Company</label>
+        <input type="text" name="company" required/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
---
+++ src/pages/components/Nav.jsx
@@ -0,0 +1,9 @@
+export default function Nav() {
+  return(
+    <nav>
+      <h1>Logo</h1>
+      <a>Login</a>
+      <a href="users/create">Register</a>
+    </nav>
+  )
+}
