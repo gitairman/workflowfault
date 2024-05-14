1c5602964e462d00a64910546911edac983722f6
Amrinder Singh <amrinder995@gmail.com>
2024-05-14 15:16:39 -0400

    remove debugging statements

--- src/pages/api/userid.js
+++ src/pages/api/userid.js
@@ -3,7 +3,6 @@ import { getUserEmailFromCookie } from '../../lib/users';
 
 export const GET = ({ cookies }) => {
   const userEmail = getUserEmailFromCookie(cookies);
-  console.log(cookies);
   if (!userEmail) {
     return new Response(JSON.stringify({ error: "Unauthorized." }), {
       status: 401,
@@ -18,7 +17,6 @@ export const GET = ({ cookies }) => {
 export const POST = async ({ request }) => {
   const userEmail = await request.json();
   const projects = await getProjectByUser(userEmail);
-  console.log(projects);
   return new Response(JSON.stringify(projects), {
     status: 200,
   });
