0ea05af4c5465164a9ec19ba119fd5ba4b86404b
Amrinder Singh <amrinder995@gmail.com>
2024-05-15 18:03:47 -0400

    add user directory nav link

--- src/pages/components/_Nav.jsx
+++ src/pages/components/_Nav.jsx
@@ -37,7 +37,9 @@ export default function Nav() {
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
         </button>
       </div>
-      <div className={`${menuOpen ? "block" : "hidden"} w-fit block lg:flex lg:items-center lg:w-auto`} >
+      <div className={`${
+          menuOpen ? "block" : "hidden"
+        } w-fit block lg:flex lg:items-center lg:w-auto`} >
         <div className="w-fit text-sm ">
         {isLoggedIn ? (
         <span className="block mt-4 lg:inline-block lg:mt-0 text-yellow-200 hover:text-white mr-4">Logged in as {userEmail}</span>
