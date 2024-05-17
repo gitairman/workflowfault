d59835ffb3e0304bf3bd56a47cac8f4b2934fa0c
Amrinder Singh <amrinder995@gmail.com>
2024-05-17 17:06:26 -0400

    fix classNames

--- src/pages/components/_ChatBoxSSE.jsx
+++ src/pages/components/_ChatBoxSSE.jsx
@@ -57,9 +57,9 @@ export default function ChatBoxSSE( { projectId }) {
       <div className="bg-gray-900 shadow-md rounded-lg max-w-full w-full h-full">
         <div className="p-4 border-b bg-yellow-500 text-white rounded-t-lg flex justify-between items-center h-[50px]">
           <p className="text-lg font-semibold">Project Chat</p>
-          <form class="max-w-sm mx-0">
-            <select onChange={handleChat} id="chat_color" class="bg-gray-900 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-              <option value="gray" selected>Default</option>
+          <form className="max-w-sm mx-0">
+            <select onChange={handleChat} id="chat_color" className="bg-gray-900 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option value="gray">Default</option>
               <option value="red">Red</option>
               <option value="green">Green</option>
               <option value="yellow">Yellow</option>
